dde34984023036c74ca261064615b8609f38e0d9
nimoissa <issanimo8@gmail.com>
2023-01-24 09:13:12 +0300

    Add postgresql database

--- fdc/settings.py
+++ fdc/settings.py
@@ -88,8 +88,9 @@ WSGI_APPLICATION = 'fdc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nimoissa',
+        'PORT':'5433',
     }
 }
 
