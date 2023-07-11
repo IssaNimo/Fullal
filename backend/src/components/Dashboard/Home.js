65971b194d106efa26bdf79fd60fa21e47901a7a
nimoissa <issanimo8@gmail.com>
2023-07-11 11:17:30 +0300

    changes

--- backend/src/components/Dashboard/Home.js
+++ backend/src/components/Dashboard/Home.js
@@ -1,78 +1,44 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import axios from 'axios'
-import './SideBar.css'
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './SideBar.css';
 
 export default function Home() {
-  const [sub_county, setSubCounty] = useState([])
-
-  const fetchSubCounty = async () => {
-
-    const result = await axios.get('http://localhost:8001/api/subcounty/')
-
-    console.log(result.data)
-    setSubCounty(result.data)
-  }
+  const [subCounties, setSubCounties] = useState([]);
 
   useEffect(() => {
-    fetchSubCounty()
-  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+    const fetchSubCounties = async () => {
+      const result = await axios.get('http://localhost:8001/api/subcounty/');
+      setSubCounties(result.data);
+    };
+    fetchSubCounties();
+  }, []);
 
+  const countyTypes = subCounties.flatMap(dataItem => [dataItem.subcounty_name])
+   .filter((countyType, index, array) => array.indexOf(countyType) === index)
+   .sort();
 
-  const countyTypes = sub_county.map(dataItem => dataItem.subcounty_name)
-  .filter((countyType, index, array) => array.indexOf(countyType) === index)
-  
-  let counts = countyTypes
-  .map(countyType => ({
-    type: countyType,
-    count: sub_county.filter(item => item.subcounty_name === countyType).length
-  }));
-console.log(counts) 
+  const counts = countyTypes.reduce((acc, countyType) => {
+    const count = subCounties.filter(item => item.subcounty_name === countyType).length;
+    acc[countyType] = count;
+    return acc;
+  }, {});
 
   return (
     <div>
       <section className=''>
-      <h1 className='section_title'>
-        <span>Number of Girls Registred in Each Sub County</span>
-      </h1>
-      
-      <div className=''>
-        
-        <div className='sub_counties SamburuNorth'>
-          <h2 className='counties_title'>
-          {/* {countyTypes
-              .map((countyType) =>
-                sub_county
-                .filter((item) => item.subcounty_name === countyType)
-                .map((countyType) => countyType.subcounty_name)
-
-              )
-            .map(countyType => (<span>{countyType}</span>))} */}
-          </h2>
-          <p>
-            {/* {countyTypes
-              .map((countyType) =>
-                sub_county.filter((item) => item.subcounty_name === countyType).length
-              )
-            .map(countyType => (<span>{countyType} </span>))} */}
-                        <h2 className='counties_title'> Samburu North</h2>
-
-          </p>
+        <h1 className='section_title'>
+          <span>Number of Girls Registered in Each Sub County</span>
+        </h1>
+        <div className=''>
+          {countyTypes.map(countyType => (
+            <div className='sub_counties' key={countyType}>
+              <h2 className='counties_title'>{countyType}</h2>
+              <p>{counts[countyType]}</p>
+            </div>
+          ))}
         </div>
-        <div className='wrapper'>
-          <div className=' sub_counties SamburuEast'>
-            <h2 className='counties_title'> Samburu East</h2>
-            <p></p>
-          </div>
-          <div className='sub_counties SamburuWest'>
-            <h2 className='counties_title'> Samburu West</h2>
-            <p></p>
-          </div>
-          
-        </div>
-      </div>
       </section>
     </div>
-  )
+  );
 }
